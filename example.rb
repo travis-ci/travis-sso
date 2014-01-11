f13a822ac12e04d2677783f4b500ab35ddb10d14
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-01-11 15:24:19 +0100

    remove 2fa from example

--- example.rb
+++ example.rb
@@ -4,9 +4,6 @@ require 'sinatra'
 secrets = {}
 
 configure do |c|
-  c.set :sso, # two factor auth
-    get_otp_secret: -> u   { secrets[u['login']]     },
-    set_otp_secret: -> u,s { secrets[u['login']] = s }
   c.register Travis::SSO
 end
 
