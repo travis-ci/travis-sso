c793237fcb0ce16651d371fa38b37efe408b83be
Tahsin Hasan <tahsin352@yahoo.com>
2020-09-28 18:39:51 +0600

    vcs provider added

--- lib/travis/sso/callback.rb
+++ lib/travis/sso/callback.rb
@@ -3,7 +3,7 @@ require 'travis/sso'
 module Travis
   module SSO
     class Callback < Generic
-      OPTIONAL_CALLBACKS = [:authorized?, :whitelisted?, :get_otp_secret, :set_otp_secret, :describe_otp, :generate_otp_secret]
+      OPTIONAL_CALLBACKS = [:authorized?, :whitelisted?, :get_otp_secret, :set_otp_secret, :describe_otp, :generate_otp_secret, :get_provider]
 
       def initialize(app, options = {})
         check = CALLBACKS - OPTIONAL_CALLBACKS
